fd4aff8dacf30c914e9d3acbbe6e3cdd9c9baaa8
Lita Stephenson <lita.stephenson@gmail.com>
2017-09-19 17:24:44 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,17 @@
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  funding goal INTEGER,
+  start_date INTEGER,
+  end_date INTEGER
+);
+
+
+CREATE TABLE users (
+  
+);
+
+
+CREATE TABLE pledges (
+  
+);
